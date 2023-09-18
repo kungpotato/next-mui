fd92e044f668d252b3d8d600bb25f3ebd6d39c9f
kungpotato <kittisak_p@scasset.com>
2023-09-18 11:12:09 +0700

    ok

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ['next/core-web-vitals', 'airbnb-base'],
+  rules: {
+    'max-len': 'off',
+    semi: 'off',
+    'no-undef': 'off',
+    'comma-dangle': 'off',
+    quotes: 'off',
+    'import/extensions': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-unused-vars': 'warn',
+    'import/order': 'off',
+    'object-curly-newline': 'off',
+    'consistent-return': 'off',
+    'no-return-assign': 'off'
+  }
+}
